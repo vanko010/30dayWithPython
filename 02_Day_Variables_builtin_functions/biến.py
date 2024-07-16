89825ba68c9c79b731df8b8e82b43ca56fdfc648
vanko010 <ld.vu000@gmail.com>
2024-07-16 10:10:11 +0700

    BT day 2

---
+++ 02_Day_Variables_builtin_functions/biến.py
@@ -0,0 +1,29 @@
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+country = 'Finland'
+city = 'Helsinki'
+age = 250
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+    'fistname':'Vu',
+    'lastname':'Le',
+    'country':'Vietnam',
+    'city':'Ho Chi Minh'
+}
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+
+first_name = input('What is your name?')
+age = input('How old are you?')
+print(first_name)
+print(age)
