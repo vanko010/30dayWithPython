1d472bf7a54721399857e839511780fe31ab2407
vanko010 <ld.vu000@gmail.com>
2024-07-16 17:43:48 +0700

    Day3-BT

---
+++ 03_Day_Operators/BT-day3.py
@@ -0,0 +1,33 @@
+#Câu 1
+age = 100.1010
+age = int(age)
+print(age)
+
+#Câu 2
+height = 1.59
+h=float(height)
+print(h)
+
+#Câu 3
+x = 10+100j
+print(type(x))
+
+#Câu 4
+base = input("Enter base:")
+b=float(base)
+height = input("Enter height:")
+h=float(height)
+area=0.5 * b * h
+a=int(area)
+print("The area of the triangle is",a)
+
+#Câu 5
+a=input("Enter side a:")
+a=float(a)
+b=input("Enter side b:")
+b=float(b)
+c=input("Enter side c:")
+c=float(c)
+perimeter=a + b +c
+p=int(perimeter)
+print("The perimeter of the triangle is",p)
