bf3b3a587f6272f3d255c172f4bf9b7dfd5e01f7
vanko010 <ld.vu000@gmail.com>
2024-07-25 10:38:43 +0700

    Push BTTH 2

--- BT_thanhhanh2.py
+++ BT_thanhhanh2.py
@@ -18,22 +18,25 @@ for soluong in range(soluongdb):
                 }
     db.append(danhsach)
 
-print ("=================================")
-timkiem = input("Tìm thông tin:")
-def checkgiong(str1,str2):
-    return difflib.SequenceMatcher(None,str1,str2).ratio()
-for danhsach in db:
-    hovaten = danhsach["firstname"]+" "+danhsach["lastname"]
-    tenvaho=danhsach["lastname"]+" "+danhsach["firstname"]
-    timkiem2 = checkgiong(timkiem,hovaten)
-    timkiem3= checkgiong(timkiem,tenvaho)
-    if timkiem2 >= 0.5:
-        print("Độ giống nhau của thông tin:",timkiem2)
-        for key, value in danhsach.items():
-            if value.strip():
-                print(f"{key}:{value}")
-    if timkiem3 >= 0.5:
-        print("Độ giống nhau của thông tin:", timkiem3)
-        for key, value in danhsach.items():
-            if value.strip():
-                print(f"{key}:{value}")
+print(db)
+print(type(db))
+
+# print ("=================================")
+# timkiem = input("Tìm thông tin:")
+# def checkgiong(str1,str2):
+#     return difflib.SequenceMatcher(None,str1,str2).ratio()
+# for danhsach in db:
+#     hovaten = danhsach["firstname"]+" "+danhsach["lastname"]
+#     tenvaho=danhsach["lastname"]+" "+danhsach["firstname"]
+#     timkiem2 = checkgiong(timkiem,hovaten)
+#     timkiem3= checkgiong(timkiem,tenvaho)
+#     if timkiem2 >= 0.5:
+#         print("Độ giống nhau của thông tin:",timkiem2)
+#         for key, value in danhsach.items():
+#             if value.strip():
+#                 print(f"{key}:{value}")
+#     if timkiem3 >= 0.5:
+#         print("Độ giống nhau của thông tin:", timkiem3)
+#         for key, value in danhsach.items():
+#             if value.strip():
+#                 print(f"{key}:{value}")
--- BT_thuchanh4.py
+++ BT_thuchanh4.py
@@ -3,20 +3,27 @@ import json
 
 with open("db.json", "r", encoding='utf-8') as f:
     db = json.load(f)
-db_dict = {(item['lastname'],item['firstname'],item['birthday'],item['sex'], item['phone'],item['email']): item for item in db}
-print(type(db_dict))
 def checkgiong(str1, str2):
     return difflib.SequenceMatcher(None, str1, str2).ratio()
 timkiem = input("Nhập thông tin tìm kiếm:")
 timkiem1 = timkiem.split()
-timkiem1 = str(timkiem1)
-for data in db_dict:
+print("====================")
+db_out=[]
+for data in db:
     hoten= data["lastname"]+" "+ data["firstname"]
     hoten1= hoten.split()
-    hoten1=str(hoten1)
     sshovaten= checkgiong(timkiem1, hoten1)
-    if sshovaten >= 0.6:
+    if sshovaten >= 0.5:
         print("Độ giống nhau của thông tin tìm kiếm:", sshovaten)
-        for key, value in item.items():
+        record={}
+        for key, value in data.items():
             if value.strip():
                 print(f"{key}:{value}")
+                record[key]=value
+        db_out.append(record)
+print(db_out)
+if len(db_out)!= 0:
+    with open("db_out.json", "w", encoding='utf-8') as file:
+        json.dump(db_out,file,ensure_ascii=False,indent=4)
+else:
+    print("Không ghi file vì file rỗng")
